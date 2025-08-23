261881de108a632deac99d57e24d4f37fb6e3d68
David <vidpebe@gmail.com>
2025-08-22 20:14:38 -0400

    practica4_dpw

--- src/main.js
+++ src/main.js
@@ -3,3 +3,10 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
